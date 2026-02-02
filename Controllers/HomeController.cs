7652d2301b275972c49f3ec1c51b7f014cfb1720
Kumarasinghe M.G.H. <it21304538@my.sliit.lk>
2026-02-02 22:43:59 +0530

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
+
 //already written the controllers for the pre- made web pages
 //home , index, and privacy page
 namespace EnigmaQuest.Controllers
