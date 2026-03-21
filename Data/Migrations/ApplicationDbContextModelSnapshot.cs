da46f6a07204e32a950cd1f89765b66eae4b4ac1
Kumarasinghe M.G.H. <hirudakakumarasingha@gmail.com>
2026-03-21 23:33:50 +0530

    Update ApplicationDbContextModelSnapshot.cs

--- Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -7,7 +7,6 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
-
 namespace EnigmaQuest.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
